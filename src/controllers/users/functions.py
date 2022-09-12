7776175ea9e662beafa26a7f587ad392d90ef88f
João Kevin <kevingomesr@gmail.com>
2022-09-12 13:39:14 -0300

    feat: http rest in vscode

--- src/controllers/users/functions.py
+++ src/controllers/users/functions.py
@@ -1,3 +1,4 @@
+import uuid
 from flask import jsonify
 from src.models.users import *
 
@@ -14,3 +15,27 @@ def getAllUsers():
 
         }
     )
+
+def createUser(jsonUser): 
+        try:
+          name = jsonUser["name"]
+          description = jsonUser["description"]
+          profile_url = jsonUser["profile_url"]
+          nickname = jsonUser["nickname"]
+          newUser = Users(id=uuid.uuid4(),name = name, description = description, profile_url = profile_url, nickname = nickname)
+          db.session.add(newUser)
+          db.session.commit()
+          return jsonify(
+                {
+                    "success": True,
+                    "message": "Successfully added",
+                }
+            )
+        except Exception as e:
+            print(str(e))
+            return jsonify(
+                {
+                    "success": False,
+                    "message":"Failed to add"
+                }
+            )
--- src/controllers/users/routes.py
+++ src/controllers/users/routes.py
@@ -1,6 +1,12 @@
+from flask import request
 from src.controllers.users.functions import *
 from src.server.instance import app
 
-@app.route("/users", methods=["GET"])
+@app.route('/users', methods=["GET"])
 def getAllForUser():
     return getAllUsers()
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    return createUser(data)
